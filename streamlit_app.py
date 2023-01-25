25d0fbcaec8887b18f2d6d21906fb6afae8fc9c0
gabrielgmendonca <gabrielgmendonca@poli.ufrj.br>
2023-01-25 17:54:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,29 @@ import wikipedia as wiki
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import nltk
+from nltk import tokenize
+from nltk.corpus import stopwords
+from nltk.stem import RSLPStemmer
 
 nltk.download('stopwords')
+nltk.download('rslp')
+nltk.download('punkt')
 
 wiki.set_lang('pt')
 title = st.text_input('Artigo da Wikipédia', 'Roda de samba')
-text = wiki.page(title).content
-extra_sw = []
+extra_sw = st.text_input('Stop words adicionais', '')
 
+text = wiki.page(title).content
+
+text = tokenize.word_tokenize(text, language='portuguese')
 custom_sw = nltk.corpus.stopwords.words('portuguese')
 custom_sw += title.split()
 custom_sw += extra_sw
-wordcloud = WordCloud(stopwords=custom_sw).generate(text)
+text = [RSLPStemmer().stem(w) for w in text if w not in custom_sw]
+
+wordcloud = WordCloud().generate(' '.join(text))
 
 fig, ax = plt.subplots()
 ax.imshow(wordcloud, interpolation='bilinear')
 ax.axis("off")
-# plt.show()
 st.pyplot(fig)
