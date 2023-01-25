c05d03d0fefc210872818b952ac033464aa728f9
gabrielgmendonca <gabrielgmendonca@poli.ufrj.br>
2023-01-25 17:31:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,23 @@
+import streamlit as st
+import pandas as pd
+import wikipedia as wiki
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import nltk
 
+nltk.download('stopwords')
+
+wiki.set_lang('pt')
+title = 'Roda de samba'
+text = wiki.page(title).content
+extra_sw = []
+
+custom_sw = stopwords.words('portuguese')
+custom_sw += title.split()
+custom_sw += extra_sw
+wordcloud = WordCloud(stopwords=custom_sw).generate(text)
+
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+st.pyplot()
